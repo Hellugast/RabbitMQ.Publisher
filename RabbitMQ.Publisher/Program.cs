8d652d45f7e14e2b5ef29e08b25c7d20d5b0e0d6
Batu YURT <asdasd018@hotmail.com>
2023-06-19 21:56:54 +0300

    Basic publisher tested

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
 Console.WriteLine("Hello, World!");
+
+//Bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://lhlfzvgo:***@crow.rmq.cloudamqp.com/lhlfzvgo");
+
+//Bağlantı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue oluşturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Queue mesaj gönderme -- Mesajlar byte türünde olmalı
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(100);
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+}
+
+
+Console.ReadLine();
+
