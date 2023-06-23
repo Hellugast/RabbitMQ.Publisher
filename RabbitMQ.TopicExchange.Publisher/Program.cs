a0bcc1d83c11206a93b4bf1cd284edc685b9e449
Batu YURT <asdasd018@hotmail.com>
2023-06-23 23:54:09 +0300

    Topic Exchange Publisher Behaviour

---
+++ RabbitMQ.TopicExchange.Publisher/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://lhlfzvgo:***@crow.rmq.cloudamqp.com/lhlfzvgo");
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "topic-exchange-example",
+    type: ExchangeType.Topic
+    );
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
+    Console.WriteLine("Topic belirtiniz");
+    string topic = Console.ReadLine();
+    channel.BasicPublish(
+        exchange: "topic-exchange-example",
+        routingKey: topic,
+        body: message
+        );
+}
+
+
+Console.ReadLine();
+
