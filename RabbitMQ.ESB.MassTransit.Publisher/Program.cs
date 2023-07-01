e08d89cecf4a641b7fe45bab3db44293a8e6e6be
Batu YURT <asdasd018@hotmail.com>
2023-07-01 23:51:48 +0300

    Enterprise Service Bus & MassTransit Configurations and Basics

---
+++ RabbitMQ.ESB.MassTransit.Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MassTransit;
+using RabbitMQ.ESB.MassTransit.Shared.Messages;
+
+Console.WriteLine("Hello, World!");
+
+
+string rabbitMQUri = "amqps://lhlfzvgo:***@crow.rmq.cloudamqp.com/lhlfzvgo";
+string queueName = "example-queue";
+
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(rabbitMQUri);
+});
+
+ISendEndpoint sendEndpoint = await bus.GetSendEndpoint(new($"{rabbitMQUri}/{queueName}"));
+
+Console.WriteLine("Gönderilecek mesaj : ");
+string message = Console.ReadLine();
+
+await sendEndpoint.Send<IMessage>(new ExampleMessage()
+{
+    Text = message
+});
+
+Console.ReadLine();
